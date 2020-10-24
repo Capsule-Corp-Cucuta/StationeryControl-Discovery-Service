27bd847717ff7459d8059e88314b3f7a5ad6aec4
Sergio Rodriguez <sergioarodram@gmail.com>
2020-10-24 00:58:29 -0500

    updated api in general

--- src/main/java/co/gov/ids/stationerycontrol/eureka/Service.java
+++ src/main/java/co/gov/ids/stationerycontrol/eureka/Service.java
@@ -4,13 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-/**
- * Discovery Service for stationery control of live births and deaths.
- *
- * @author Sergio Rodríguez
- * @version 0.0.1
- * @since 2020
- */
 @EnableEurekaServer
 @SpringBootApplication
 public class Service {
